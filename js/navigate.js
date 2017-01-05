aaca5c100435964b7816ae2fc3e74acb631bf116
Nicolas Emery <Nicolas5150@gmail.com>
2017-01-04 22:15:17 -0500

    Original Push
    Portfolio site for Nicolas - first push.

---
+++ js/navigate.js
@@ -0,0 +1,30 @@
+window.addEventListener('load',remove,false);
+
+// Remove all but image galery from the page.
+function remove() {
+  contentRemove(1);
+}
+
+function contentRemove(value) {
+  var contentSections = document.getElementsByClassName('content');
+  for (i = value; i < contentSections.length; i ++) {
+    contentSections[i].style.display = 'none';
+  }
+
+  return contentSections;
+}
+
+function changeSection (showID) {
+  // Remove all items on screen.
+  var home = document.getElementById("wrapper_home");
+  home.style.display = "none";
+  var contentSections = contentRemove(0);
+
+  // Fade in new item
+  $(contentSections[showID]).fadeIn(1500);
+}
+
+// Mobile navbar dropdown.
+function navbar() {
+    $("#moving_bar").slideToggle("fast");
+}
