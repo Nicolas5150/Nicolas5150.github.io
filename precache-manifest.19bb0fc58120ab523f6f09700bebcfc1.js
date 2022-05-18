608e36a1898d36fef32942f2af7d96267ae8a991
Nicolas Emery <Nicolas5150@gmail.com>
2022-05-18 14:09:47 -0700

    Updates

---
+++ precache-manifest.19bb0fc58120ab523f6f09700bebcfc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262b9d4b4c7db221ede67d1d5e396b71",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/index.html"
+  },
+  {
+    "revision": "64a20e2ab34c9641572f",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "68c155f7b199c4563bf9",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/css/main.c5782a46.chunk.css"
+  },
+  {
+    "revision": "64a20e2ab34c9641572f",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/js/2.22ebf169.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/js/2.22ebf169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c155f7b199c4563bf9",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/js/main.09708277.chunk.js"
+  },
+  {
+    "revision": "defabb46ecd304ff4ff0",
+    "url": "/Nicolas5150.github.io/Nicolas5150.github.io/static/js/runtime-main.c0cd068f.js"
+  }
+]);
