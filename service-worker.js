cae062dee5188515807da1957eedf4209e93a989
Nicolas Emery <Nicolas5150@gmail.com>
2022-05-18 14:28:05 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Nicolas5150.github.io/Nicolas5150.github.io/precache-manifest.19bb0fc58120ab523f6f09700bebcfc1.js"
+  "./precache-manifest.1c47cb98a3d74588127760d8ee42e746.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nicolas5150.github.io/Nicolas5150.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
