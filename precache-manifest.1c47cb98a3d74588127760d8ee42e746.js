56c2119d0606aa67c2f9d3968e4891c71aca604d
Nicolas Emery <Nicolas5150@gmail.com>
2022-05-18 13:49:54 -0700

    Updates

---
+++ precache-manifest.1c47cb98a3d74588127760d8ee42e746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7e5066189b4973be48c9eafa8a2ac9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64a20e2ab34c9641572f",
+    "url": "./static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "406b88dfc2ef2af2b2d0",
+    "url": "./static/css/main.c5782a46.chunk.css"
+  },
+  {
+    "revision": "64a20e2ab34c9641572f",
+    "url": "./static/js/2.22ebf169.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.22ebf169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406b88dfc2ef2af2b2d0",
+    "url": "./static/js/main.498c6c43.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
