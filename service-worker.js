e34223485ceb8d333f889dc2d51aa95b99769c10
Nicolas Emery <Nicolas5150@gmail.com>
2022-05-19 01:21:09 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d156ebdb353ee234ef362b4c0d659343.js"
+  "./precache-manifest.11ffb8976d8f0ffaca54e6d27eba60c6.js"
 );
 
 self.addEventListener('message', (event) => {
